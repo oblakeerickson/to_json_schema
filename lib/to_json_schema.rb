8d57f0b74a8fa98a78248d3241df7eee3f8e947b
Blake Erickson <o.blakeerickson@gmail.com>
2021-01-21 19:11:16 -0700

    Figured out how to parse the input hash
    Added the ability to walk nested objects by adding recursion if it
    detects another object.

--- lib/to_json_schema.rb
+++ lib/to_json_schema.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
 require 'json'
 
 module ToJsonSchema
   class Converter
+
+    attr_reader :schema
 
     def initialize(args)
       @args = args
@@ -9,23 +12,67 @@ module ToJsonSchema
       self.add_defaults
     end
 
-    def convert
+    def print
+      puts JSON.pretty_generate(@schema)
+    end
+
+    def load_content
       if @args[:input]
-        contents = self.read_file
-        puts contents
-        contents.keys.each do |k|
-          if contents[k].is_a? Hash
-            @schema["properties"] = {
-              "#{k}": {
-                type: "object",
-                properties: {}
-              }
-            }
-          end
-        end
-        puts @schema
-        puts @schema.to_json
+        content = self.read_file
+      elsif
+        content = @args[:string]
       end
+    end
+
+    def run
+      content = self.load_content
+      @schema[:properties], @schema[:required] = walk(content)
+    end
+
+    def walk(object)
+      required = []
+      h = {}
+      object.each do |k,v|
+        required << k.to_s
+        type = get_type(v)
+        if type == "object"
+          p, r = walk(object[k])
+          h[k.to_sym] = {
+            type: type,
+            properties: p,
+            required: r
+          }
+        elsif type == "array"
+          h[k.to_sym] = {
+            type: type,
+            items: []
+          }
+        else
+          h[k.to_sym] = {
+            type: type
+          }
+        end
+      end
+
+      [h, required]
+    end
+
+    def get_type(value)
+      type = ""
+      if value.is_a? Integer
+        type = "integer"
+      end
+      if value.is_a? Hash
+        type = "object"
+      end
+      if value.is_a? String
+        type = "string"
+      end
+      type = "boolean" if value == true || value == false
+      if value.is_a? Array
+        type = "array"
+      end
+      type
     end
 
     def read_file
@@ -34,15 +81,10 @@ module ToJsonSchema
 
     def add_defaults
       self.add_additional_properties_field
-      self.add_properties_field
     end
 
     def add_additional_properties_field
       @schema["additionalProperties"] = false
-    end
-
-    def add_properties_field
-      @schema["properties"] = {}
     end
 
   end
